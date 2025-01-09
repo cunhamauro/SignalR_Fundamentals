9e60bec5062229174485548219ad3e680ddb4ba4
Mauro Cunha <cunhamauro@outlook.pt>
2025-01-09 15:14:41 +0000

    Add project files.

---
+++ SignalRtutorial/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SignalRtutorial.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
