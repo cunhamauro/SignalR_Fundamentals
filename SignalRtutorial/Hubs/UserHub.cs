7fda835c6982aebd3c402274b38ac290c64f3ec3
Mauro Cunha <cunhamauro@outlook.pt>
2025-01-09 16:16:58 +0000

    SignalR setup

---
+++ SignalRtutorial/Hubs/UserHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRtutorial.Hubs
+{
+    public class UserHub : Hub
+    {
+        public static int TotalViews { get; set; } = 0;
+
+        public async Task NewWindowLoaded()
+        {
+            TotalViews++;
+
+            // Notify clients that the count was updated
+            await Clients.All.SendAsync("updateTotalViews", TotalViews); // Target a method and pass the updated count
+        }
+    }
+}
--- SignalRtutorial/Program.cs
+++ SignalRtutorial/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using SignalRtutorial.Data;
+using SignalRtutorial.Hubs;
 
 namespace SignalRtutorial
 {
@@ -19,6 +20,8 @@ namespace SignalRtutorial
             builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             builder.Services.AddControllersWithViews();
+
+            builder.Services.AddSignalR();
 
             var app = builder.Build();
 
@@ -45,6 +48,8 @@ namespace SignalRtutorial
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
             app.MapRazorPages();
+
+            app.MapHub<UserHub>("/hubs/userCount");
 
             app.Run();
         }
